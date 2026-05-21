fa065f61503c4ae7479f4fc5fb7316622b9bd5b1
Eric Porter <eric@sporter.net>
2026-05-20 18:20:04 -0700

    added three tests for basic satellite orbits: 0, 45, & 90 degree orbits

--- tests/satelliteTests.cpp
+++ tests/satelliteTests.cpp
@@ -1,17 +1,41 @@
 #include "../src/sim/Satellite.hpp"
+#include "../src/util/constants.hpp"
 
 #include <gtest/gtest.h>
 
 #include <iostream>
 
-TEST(SatelliteTests, EquatorialOrbit)
+
+//6800km is the avg distance of the ISS from the earth's center, which is being used for the following tests
+TEST(SatelliteTests, EquatorialOrbitZeroAnomaly)
 {
-    std::cout << "TEMP";
+    Satellite test(6.8e6,0,0);
+    test.updatePosition(1, 0);
+
+    const float expected[3] = {6.79999569e6,7.655984675e3,0};
+    const float* actual = test.getPosition();
+
+    for(int i = 0; i < 3; ++i) EXPECT_NEAR(actual[i],expected[i],.01);
 }
 
-TEST(SatelliteTests, RadiusStaysConstant)
+TEST(SatelliteTests, 45DegreeInclinationZeroAnomaly)
 {
-    float radius = 6999.95f;
+    Satellite test(6.8e6,PI/4,0);
+    test.updatePosition(1, 0);
 
-    EXPECT_NEAR(radius, 7000.0f, 0.1f);
+    const float expected[3] = {6.79999569e6,5.413598681e3,5.41359868e3};
+    const float* actual = test.getPosition();
+
+    for(int i = 0; i < 3; ++i) EXPECT_NEAR(actual[i],expected[i],.01);
+}
+
+TEST(SatelliteTests, PolarOrbitZeroAnomaly)
+{
+    Satellite test(6.8e6,PI/2,0);
+    test.updatePosition(1, 0);
+
+    const float expected[3] = {6.79999569e6,0,7.655984675e3};
+    const float* actual = test.getPosition();
+
+    for(int i = 0; i < 3; ++i) EXPECT_NEAR(actual[i],expected[i],.01);
 }
