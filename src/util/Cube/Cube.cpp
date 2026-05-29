26428848e5494d51f4bc610ce1e2dd7a8506dcb8
Eric Porter <eric@sporter.net>
2026-05-29 15:44:19 -0700

    started cube class

--- src/util/Cube/Cube.cpp
+++ src/util/Cube/Cube.cpp
@@ -1,1 +1,38 @@
 #include "Cube.hpp"
+
+Cube::Cube(float length, std::initializer_list<float> color)
+{
+    generateVertices(length, color);
+    generateIndexBuffer();
+}
+
+void Cube::generateVertices(float length, std::initializer_list<float> color)
+{
+    generateSquare(length, 0, color);
+    generateSquare(length, length, color);
+}
+
+void Cube::generateSquare(float length, float depth, std::initializer_list<float> color)
+{
+    pushVertex({0,0,depth},color);
+    pushVertex({0,length,depth},color);
+    pushVertex({length,0,depth},color);
+    pushVertex({length,length,depth},color);
+}
+
+void Cube::pushVertex(std::initializer_list<float> point, std::initializer_list<float> color)
+{
+    for(float x : point) vertices.push_back(x);
+    for(float x : color) vertices.push_back(x);
+}
+
+void Cube::generateIndexBuffer()
+{
+    for(int i = 0; i < 8; ++i)
+    {
+        indexBuffer.push_back(i);
+        indexBuffer.push_back((i + 1) % 8);
+        indexBuffer.push_back((i + 2) % 8);
+    }
+    //TODO: add the top & bottom faces
+}
--- src/util/Cube/Cube.hpp
+++ src/util/Cube/Cube.hpp
@@ -1,1 +1,20 @@
 #pragma once
+#include <initializer_list>
+#include <vector>
+
+class Cube
+{
+public:
+    Cube(float length, std::initializer_list<float> color);
+private:
+    void generateVertices(float length, std::initializer_list<float> color);
+    void generateSquare(float length, float depth, std::initializer_list<float> color);
+    void pushVertex(std::initializer_list<float> point, std::initializer_list<float> color);
+    void generateIndexBuffer();
+
+    std::vector<float> vertices;
+    std::vector<unsigned int> indexBuffer;
+    unsigned int VAO;
+    unsigned int VBO;
+    unsigned int EBO;
+};
